8a8de24ec5dc123c34713679c39d33a4de919b94
James Hayes <exajames@gmail.com>
2024-01-12 14:17:57 +0000

    updates

--- js/custom.js
+++ js/custom.js
@@ -18,26 +18,73 @@ function bindToButtom() {
         console.log("Button: " + this)
         const image = document.getElementById('zoom-image')
         image.classList.add('zoom-image-clicked');
-        const container = document.querySelectorAll('#home .container')[1]
+        const container = document.querySelectorAll('#home .overlay-div2')[0]
         container.setAttribute("style", "display: none")
     })
-    document.querySelectorAll('section').
-     forEach((element) => {
-        element.setAttribute("style", "display: none")
-     })
-    document.querySelectorAll('footer').
-     forEach((element) => {
-        element.setAttribute("style", "display: none")
-     })
-    document.querySelectorAll('navbar').
-     forEach((element) => {
-        element.setAttribute("style", "display: none")
-     })
-     const section = document.getElementById('home')
-     section.setAttribute("style", "display: block")
+    const keys = [
+        'section',
+        'footer',
+        'navbar'
+    ]
+    const names = [
+        'style',
+        'style',
+        'style'
+    ]
+    const values = [
+        'display: none;',
+        'display: none;',
+        'display: none;',
+    ]
+    const saved = []
+    function changeAttribute(key, value, get, set) {
+        const origvalue = saved[key]
+        if (origvalue != value) {
+            saved[key] = get()
+        }
+        set(value)
+    }
+    function changeAttributeList(index, flag) {
+        if (index < keys.length) {
+            const key = keys[index] + names[index]
+            console.log(`processing ${key}`)
+            function getNewValue() {
+                if (flag) {
+                    return values[index]
+                } else {
+                    return saved[key]
+                }
+            }
+            const newvalue = getNewValue()
+            document.querySelectorAll(keys[index]).
+             forEach((element) => {
+                changeAttribute(key, newvalue,
+                    () => {
+                        return element.getAttribute(names[index])
+                    },
+                    (newvalue) => {
+                        element.setAttribute(names[index], newvalue)
+                    })
+             })
+             changeAttributeList(index + 1, flag)
+        }
+    }
+    changeAttributeList(0, true)
+
+    const section = document.getElementById('home')
+    const key = 'section' + 'style'
+    changeAttribute(key, saved[key],
+        () => {
+            return section.getAttribute('style')
+        },
+        (newvalue) => {
+            section.setAttribute('style', newvalue)
+        }
+    )
 }
 
 function scrollToTop() {
+    window.scrollTo(0, 0);
     document.documentElement.scrollTop = 0; // For modern browsers
     document.body.scrollTop = 0; // For older browsers
 }
@@ -45,7 +92,9 @@ function scrollToTop() {
 // ISOTOPE FILTER
 jQuery(document).ready(function($){
     bindToButtom()
-    scrollToTop()
+    window.setTimeout(() => {
+        scrollToTop()
+    }, 1000)
 	if ( $('.iso-box-wrapper').length > 0 ) { 
 
 	    var $container 	= $('.iso-box-wrapper'), 
